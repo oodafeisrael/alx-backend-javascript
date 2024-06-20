2ecfeda233ee98f2c69636f59531ef2e4a468e2a
oodafeisrael <oodafeisrael@yahoo.com>
2024-06-20 17:32:00 +0000

    task 1 commit

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,14 +1,32 @@
-const calculateNumber = (type, a, b) => {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
+/**
+ * Performs a mathematical operation on two rounded numbers
+ * @param {string} type - The type of operation to perform (SUM, SUBTRACT,
+ * or DIVIDE)
+ * @param {number} a - The first number to be rounded and used in the
+ * operation
+ * @param {number} b - The second number to be rounded and used in the
+ * operation
+ * @returns {number|string} The result of the operation, or "Error" if
+ * the operation is division and b is rounded to 0
+ */
+function calculateNumber(type, a, b) {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return roundedA + roundedB;
+    case 'SUBTRACT':
+      return roundedA - roundedB;
+    case 'DIVIDE':
+      if (roundedB === 0) {
+        return 'Error';
+      }
+      return roundedA / roundedB;
+
+    default:
+      throw new Error(`Invalid type: ${type}`);
   }
-  if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  }
-  if (type === 'DIVIDE') {
-    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
-  }
-  return 0;
-};
+}
 
 module.exports = calculateNumber;
